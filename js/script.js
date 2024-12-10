f32699a0f14cfcc9ee27d5bbc1844845e7ce66a9
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-10 16:59:40 +0100

    feat: get  elements

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Docs Elements
+
+// Elements dati client
+const nameElem = document.getElementById(`name`)
+const kmClientElem = document.getElementById(`km`)
+const tariffStandardElem = document.getElementById(`standard`)
+const tariffMinoreniElem = document.getElementById(`minoreni`)
+const tariffPensionatiElem = document.getElementById(`pensionati`)
+const submitElem = document.getElementById(`submit`)
+const resetELem = document.getElementById(`reset`)
+
+
+// Elements dati biglietto
+
+const nameClientElem = document.getElementById(`name-client`)
+const offertaElem = document.getElementById(`offerta`)
+const seatElem = document.getElementById(`seat`)
+const codiceElem = document.getElementById(`codice`)
+const costoBigliettoElem = document.getElementById(`costo`)
