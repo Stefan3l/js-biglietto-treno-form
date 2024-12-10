1a96b0ec41a6dc08cd0817a1695bcaad61f2d731
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-10 23:02:22 +0100

    feat: biglieto treno

--- js/script.js
+++ js/script.js
@@ -3,28 +3,89 @@
 // Elements dati client
 const nameElem = document.getElementById(`name`)
 const kmClientElem = document.getElementById(`km`)
-console.log(kmClientElem)
-const tariffStandardElem = document.getElementById(`standard`)
-const tariffMinoreniElem = document.getElementById(`minoreni`)
-const tariffPensionatiElem = document.getElementById(`pensionati`)
+const tariffAllElem = document.getElementById(`options`)
 const submitElem = document.getElementById(`submit`)
 const resetELem = document.getElementById(`reset`)
+const formElem = document.getElementById(`form`)
+
 
 
 // Elements dati biglietto
 
+const bigliettoTrenoElem = document.getElementById(`biglietto-treno`)
 const nameClientElem = document.getElementById(`name-client`)
 const offertaElem = document.getElementById(`offerta`)
+console.log(offertaElem.value)
 const seatElem = document.getElementById(`seat`)
 const codiceElem = document.getElementById(`codice`)
 const costoBigliettoElem = document.getElementById(`costo`)
 
+// dati
+
 const priceKm = 0.21;
 console.log(priceKm)
 
+const numberSeat =  Math.floor((Math.random() * 10) + 1)
+const numberCodice =  Math.floor((Math.random() * (100000 - 90000)) + 90000)
 
 
-submitElem.addEventListener("submit", function() {
-    let priceBiglieto = kmClientElem * priceKm;
-    console.log(priceBiglieto)
+
+
+// Dom events
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let priceBiglietto = kmClientElem.value * priceKm;
+    if(tariffAllElem.value === "minoreni") {      
+        const discountPriceMinoreni = priceBiglietto * 0.20;
+        discount = priceBiglietto - discountPriceMinoreni;
+        priceBiglietto -= priceBiglietto - discount;       
+        offertaElem.innerHTML = tariffAllElem.value
+        costoBigliettoElem.innerHTML = priceBiglietto.toFixed(2)
+        bigliettoTrenoElem.classList.remove(`ticket`)
+        console.log(priceBiglietto)
+    }
+    else if(tariffAllElem.value === "pensionati") {
+        const discountPricePensionati = priceBiglietto * 0.40;
+        discount = priceBiglietto - discountPricePensionati;
+        priceBiglietto -= priceBiglietto - discount;
+        offertaElem.innerHTML = tariffAllElem.value
+        costoBigliettoElem.innerHTML = priceBiglietto.toFixed(2)
+        bigliettoTrenoElem.classList.remove(`ticket`)
+        console.log(priceBiglietto)
+    } 
+    else {
+        priceBiglietto = kmClientElem.value * priceKm;
+        offertaElem.innerHTML = tariffAllElem.value
+        costoBigliettoElem.innerHTML = priceBiglietto.toFixed(2)
+        bigliettoTrenoElem.classList.remove(`ticket`)
+    }
+
+    
+    
+    
+
+    nameClientElem.innerHTML = nameElem.value; 
+    seatElem.innerHTML = numberSeat
+    codiceElem.innerHTML = numberCodice
+    
+    
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
